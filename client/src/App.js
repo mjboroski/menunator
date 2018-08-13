54ec5e99ada85af39ed009fc91140b156f0acefd
mjboroski <mjboroski@gmail.com>
2018-08-12 22:25:36 -0400

    started navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from '../components/NavBar';
 
 class App extends Component {
   render() {
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = props => {
+  return (
+    <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
+      <NavLink
+        style={{ marginRight: '10px' }}
+        to="/"
+      >
+      Home
+    </NavLink>
+  
+  </div>
+);
+}
+
+export default NavBar;
