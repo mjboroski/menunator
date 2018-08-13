cffb49f264514b378691b744d734add52c5cd435
mjboroski <mjboroski@gmail.com>
2018-08-12 22:21:59 -0400

    built router in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <NavBar />
+          <Route exact path="/" render={() => <div>Home</div>} />
+          <Route path='/menu' component={menu}/>
+          <Route path='/order' component={order}/>
+        </div>
+      </Router>
     );
   }
 }
 
-// return (
-//   <Router>
-//     <div>
-//       <NavBar />
-//       <Route exact path="/" render={() => <div>Home</div>} />
-//       <Route path='/movies' component={MoviesPage}/>
-//     </div>
-//   </Router>
-// );
-// }
-// }
 export default App;
