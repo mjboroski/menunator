559ce0ab03dfb9376811e3b8551e69be89b072bb
mjboroski <mjboroski@gmail.com>
2018-08-14 14:31:13 -0400

    ingredient input added

---
+++ client/src/components/ingredients/IngredientInput.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class IngredientInput extends Component {
+
+  state = {text: ''}
+
+  handleOnChange = event => {this.setState({text: event.target.value,});}
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    this.props.addReview({text: this.state.text, itemId: this.props.itemId });
+    this.setState({text: '',});}
+
+  render() {
+    return (
+     <div>
+        <form onSubmit={this.handleOnSubmit} >
+          <label>Add Ingredient</label>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.handleOnChange} />
+          <input type="submit" />
+        </form>
+      </div>
+    );}};
+
+export default IngredientInput;
