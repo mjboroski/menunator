e81550ad8d7966d5343a07c1845b247823265ec6
mjboroski <mjboroski@gmail.com>
2018-08-15 14:17:23 -0400

    removed registerServiceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import manageItems from './reducers/manageItems'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -15,5 +14,3 @@ ReactDOM.render(
   </Provider>,
 document.getElementById('root')
 )
-
-registerServiceWorker();
