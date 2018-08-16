16d3a892da36fee8628e28a8cf0e727f3ef0b51f
mjboroski <mjboroski@gmail.com>
2018-08-15 20:43:56 -0400

    finished actions

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -40,3 +40,71 @@ const removeItem = id => {
     id
   };
 };
+
+export const getItems = () => {
+  return dispatch => {
+    return fetch(`${API_URL}/items`)
+      .then(response => response.json())
+      .then(items => dispatch(setItems(items)))
+      .catch(error => console.log(error));
+  }
+}
+
+export const getItem = id => {
+  return dispatch => {
+    return fetch(`${API_URL}/items/${id}`)
+      .then(response => response.json())
+      .then(item => dispatch(setItem(item)))
+      .catch(error => console.log(error));
+  }
+}
+
+export const createItem = item => {
+  return dispatch => {
+    return fetch(`${API_URL}/items`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(item)
+    })
+    .then(response => response.json())
+    .then(item => {
+      dispatch(addItem(item));
+    })
+    .catch(error => console.log(error));
+  }
+}
+
+export const updateItem = (item, id) => {
+  return dispatch => {
+    return fetch(`${API_URL}/items/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(item)
+    })
+      .then(response => response.json())
+      .then(item => {
+        dispatch(editItem(item));
+      })
+      .catch(error => console.log(error));
+  };
+};
+
+export const deleteItem = id => {
+  return dispatch => {
+    return fetch(`${API_URL}/items/${id}`, {
+      method: "DELETE"
+    })
+      .then(response => {
+        if (response.ok) {
+          dispatch(removeItem(id));
+        } else {
+          window.alert("Unable to delete");
+        }
+      })
+      .catch(error => console.log(error));
+  };
+};
